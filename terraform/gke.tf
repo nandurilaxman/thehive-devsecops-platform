7e2be2f7ecdeafbd0ac1785846d1bf004d2706fe
Laxman Nanduri <162944154+nandurilaxman@users.noreply.github.com>
2026-06-18 22:31:53 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "thehive" {
   name     = "thehive-gke"
-  location = var.region
+  location = var.zone   # asia-south1-a
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -10,17 +10,19 @@ resource "google_container_cluster" "thehive" {
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-pool"
-  location   = var.region
-  cluster    = google_container_cluster.thehive.name
+  name     = "primary-pool"
+  location = var.zone
+  cluster  = google_container_cluster.thehive.name
 
-  node_count = 2
+  node_count = 1
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-micro"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
+
+  disk_size_gb = 20
 }
