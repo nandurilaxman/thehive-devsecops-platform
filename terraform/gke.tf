70d4a40711a51799cdfcc067354a103dc022eac4
Laxman Nanduri <162944154+nandurilaxman@users.noreply.github.com>
2026-06-18 22:32:19 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -17,7 +17,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   node_config {
-    machine_type = "e2-micro"
+    machine_type = "e2-small"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
